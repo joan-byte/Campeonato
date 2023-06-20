acf82889dfa9cea15d42c5176ae643a317e85b4e
JoanAlbaTort <jalba@seedcorporatefinance.com>
2023-06-20 09:43:59 +0200

    first commit

---
+++ campeonato_parejas/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for campeonato_parejas project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'campeonato_parejas.settings')
+
+application = get_wsgi_application()
