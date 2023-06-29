e54ef0400d89dc5873370fdbe4f7c8047c845658
Joan Alba <picasoca@me.com>
2023-06-29 17:12:53 +0200

    "con numero Pareja"

--- inscripcion_parejas/models.py
+++ inscripcion_parejas/models.py
@@ -6,6 +6,7 @@ class Parejas(models.Model):
     Jugador1 = models.CharField(max_length=50)
     Jugador2 = models.CharField(max_length=50)
     Club_pertenencia = models.CharField(max_length=50)
+    NUmero_pareja = models.IntegerField()   #numero de pareja
 
     def pareja(self):
         cadena = "{0} {1}"
